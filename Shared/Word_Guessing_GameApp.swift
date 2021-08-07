8e527d9f5e7d499e05e9b1d0502c53dd2883265a
Vlad Spreys <vlad@spreys.com>
2021-08-07 22:08:03 +1000

    First commit

---
+++ Shared/Word_Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_Guessing_GameApp.swift
+//  Shared
+//
+//  Created by Vlad Spreys on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
